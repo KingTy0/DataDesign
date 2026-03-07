48459da608629a54661a13f7971a127711b8c4b4
KingTy0 <tek.king999@gmail.com>
2026-03-07 15:58:17 -0600

    Add files via upload

---
+++ FINALPROJECTFORDATADESIGN/NBADATAMVC/NBADATAMVC/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+public class HomeController : Controller
+{
+    private readonly NbaDataService _data;
+
+    public HomeController(NbaDataService data) => _data = data;
+
+    public async Task<IActionResult> Index(string name, string team, int page = 1)
+    {
+        int pageSize = 20;
+        var filter = Builders<BsonDocument>.Filter.Empty;
+
+        if (!string.IsNullOrEmpty(name))
+        {
+            var nameFilter = Builders<BsonDocument>.Filter.Or(
+                Builders<BsonDocument>.Filter.Regex("player_name", new BsonRegularExpression(name, "i")),
+                Builders<BsonDocument>.Filter.Regex("name", new BsonRegularExpression(name, "i")),
+                Builders<BsonDocument>.Filter.Regex("full_name", new BsonRegularExpression(name, "i")),
+                Builders<BsonDocument>.Filter.Regex("first_name", new BsonRegularExpression(name, "i")),
+                Builders<BsonDocument>.Filter.Regex("last_name", new BsonRegularExpression(name, "i")),
+                Builders<BsonDocument>.Filter.Regex("display_first_last", new BsonRegularExpression(name, "i"))
+            );
+            filter &= nameFilter;
+        }
+
+        if (!string.IsNullOrEmpty(team))
+            filter &= Builders<BsonDocument>.Filter.Regex("team_abbreviation", new BsonRegularExpression(team, "i"));
+
+        // DELETE debug code after this works
+        var total = await _data.Players.CountDocumentsAsync(filter);
+        var players = await _data.Players.Find(filter).Skip((page - 1) * pageSize).Limit(pageSize).ToListAsync();
+
+        ViewBag.NameFilter = name;
+        ViewBag.TeamFilter = team;
+        ViewBag.Page = page;
+        ViewBag.TotalPages = (int)Math.Ceiling((double)total / pageSize);
+        return View(players);
+    }
+
+
+}
---
+++ FINALPROJECTFORDATADESIGN/NBADATAMVC/NBADATAMVC/Services/NbaDataService.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+public class NbaDataService
+{
+    private readonly IMongoDatabase _database;
+
+    public NbaDataService(IConfiguration config)
+    {
+        var client = new MongoClient(config.GetConnectionString("MongoDb"));
+        _database = client.GetDatabase("NBAData");
+    }
+
+    public IMongoCollection<BsonDocument> Players => _database.GetCollection<BsonDocument>("players");
+    public IMongoCollection<BsonDocument> Teams => _database.GetCollection<BsonDocument>("teams");
+    public IMongoCollection<BsonDocument> PlayerStats => _database.GetCollection<BsonDocument>("player_stats");
+    public IMongoCollection<BsonDocument> Games => _database.GetCollection<BsonDocument>("games");
+}
